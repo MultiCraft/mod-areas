030b27b826a1efd26a5e7091ed89b4cc2d1ab4be
MoNTE48 <MoNTE48@mail.ua>
2019-10-20 21:08:51 +0200

    Use MC HUD Api instead old API

--- hud.lua
+++ hud.lua
@@ -1,59 +1,58 @@
 -- This is inspired by the landrush mod by Bremaweb
 areas.hud = {}
 
-minetest.register_playerstep(function(dtime, playernames)
+hud.register("areas", {
+	hud_elem_type = "text",
+	position      = {x = 0,   y =  1},
+	alignment     = {x = 1,   y = -1},
+	offset        = {x = 8,   y = -8},
+	scale         = {x = 200, y = 60},
+	text          = "",
+	number        = 0xFFFFFF
+})
+
+minetest.register_playerstep(function(_, playernames)
 	for _, name in pairs(playernames) do
 		local player = minetest.get_player_by_name(name)
-		if player and player:is_player() then
-			local pos = vector.round(player:get_pos())
-			pos = vector.apply(pos, function(p)
-				return math.max(math.min(p, 2147483), -2147483)
-			end)
-			local areaStrings = {}
+		if not player or not player:is_player() then return end
+		local pos = vector.round(player:get_pos())
+		pos = vector.apply(pos, function(p)
+			return math.max(math.min(p, 2147483), -2147483)
+		end)
+		local areaStrings = {}
 
-			for id, area in pairs(areas:getAreasAtPos(pos)) do
-				table.insert(areaStrings, ("%s [%u] (%s%s)")
-						:format(area.name, id, area.owner,
-						area.open and ":open" or ""))
-			end
+		for id, area in pairs(areas:getAreasAtPos(pos)) do
+			table.insert(areaStrings, ("%s [%u] (%s%s)")
+					:format(area.name, id, area.owner,
+					area.open and ":open" or ""))
+		end
 
-			for i, area in pairs(areas:getExternalHudEntries(pos)) do
-				local str = ""
-				if area.name then str = area.name .. " " end
-				if area.id then str = str.."["..area.id.."] " end
-				if area.owner then str = str.."("..area.owner..")" end
-				table.insert(areaStrings, str)
-			end
+		for i, area in pairs(areas:getExternalHudEntries(pos)) do
+			local str = ""
+			if area.name then str = area.name .. " " end
+			if area.id then str = str.."["..area.id.."] " end
+			if area.owner then str = str.."("..area.owner..")" end
+			table.insert(areaStrings, str)
+		end
 
-			local areaString = ""
-			if #areaStrings > 0 then
-				areaString = "Areas:"
-				areaString = areaString.."\n"..
-					table.concat(areaStrings, "\n")
-			end
-			local hud = areas.hud[name]
-			if not hud then
-				hud = {}
-				areas.hud[name] = hud
-				hud.areasId = player:hud_add({
-					hud_elem_type = "text",
-					name = "Areas",
-					number = 0xFFFFFF,
-					position = {x=0, y=1},
-					offset = {x=8, y=-8},
-					text = "",
-					scale = {x=200, y=60},
-					alignment = {x=1, y=-1},
-				})
-				hud.oldAreas = areaString
-				return
-			elseif hud.oldAreas ~= areaString then
-				player:hud_change(hud.areasId, "text", areaString)
-				hud.oldAreas = areaString
-			end
+		local areaString = ""
+		if #areaStrings > 0 then
+			areaString = "Areas:"
+			areaString = areaString.."\n"..
+				table.concat(areaStrings, "\n")
+		end
+
+		if not areas.hud[name] then
+			areas.hud[name] = {}
+			hud.change_item(player, "areas", {text = areaString})
+			areas.hud[name].oldAreas = areaString
+			return
+		elseif areas.hud[name].oldAreas ~= areaString then
+			hud.change_item(player, "areas", {text = areaString})
+			areas.hud[name].oldAreas = areaString
 		end
 	end
-end, false)
+end, true) -- Force this callback to run every step for smoother animations
 
 minetest.register_on_leaveplayer(function(player)
 	areas.hud[player:get_player_name()] = nil
