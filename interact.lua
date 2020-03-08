c1530388ad047482d23bb8ec668fc9221f8df18d
Louis Royer <55180044+louisroyer@users.noreply.github.com>
2020-03-08 22:15:00 +0100

    Add translation support

--- interact.lua
+++ interact.lua
@@ -44,7 +44,7 @@ minetest.register_on_protection_violation(function(pos, name)
 	if not areas:canInteract(pos, name) then
 		local owners = areas:getNodeOwners(pos)
 		minetest.chat_send_player(name,
-			(S("%s is protected by %s.")):format(
+			S("@1 is protected by @2.",
 				minetest.pos_to_string(pos),
 				table.concat(owners, ", ")))
 
